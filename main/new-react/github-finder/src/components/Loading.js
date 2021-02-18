29ab1ba8efda3f3041ca8600901a69f58cfa9148
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2021-02-18 10:55:18 +0300

    deneme

---
+++ main/new-react/github-finder/src/components/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Loading = () => {
+  return (
+    <>
+      <img
+        alt="./logo512.png"
+        style={{ width: '200px', display: 'block', margin: 'auto' }}
+      ></img>
+    </>
+  );
+};
+
+export default Loading;
