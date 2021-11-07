bdb9a811b5f1a3ef5811855da7fcd0d10fb3236f
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2021-11-07 14:44:08 +0300

    Week2 homework on CleanBlog is finished

--- Week2/CleanBlog/app.js
+++ Week2/CleanBlog/app.js
@@ -1,14 +1,40 @@
+// imports
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
+
 const PORT = process.env.PORT || 5000;
+
+// Set static files
+app.use(express.static(__dirname + '/public'));
+
+// Set views
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
 
 // Send blog post
 app.get('/', (req, res) => {
-  res.status(200).send(blog);
+  res.status(200).render('index', {
+    blog,
+  });
+});
+// About page
+app.get('/about', (req, res) => {
+  res.status(200).render('about');
 });
 
+// Post page
+app.get('/post', (req, res) => {
+  res.status(200).render('post');
+});
+// Create post page
+app.get('/add_post', (req, res) => {
+  res.status(200).render('add_post');
+});
+
+// Server listen
 app.listen(PORT, () => {
   console.log(`Server is running by localhost:${PORT}`);
 });
